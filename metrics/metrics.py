90a670966db829d2b926d0dccd986eae13de8419
tdp1996 <phuongtd1107@gmail.com>
2024-07-03 15:25:22 +0700

    add precision metric

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,15 +1,78 @@
-def r_square(y_true: list, y_predict: list) ->float:
+import math
+from typing import Optional, Union
+
+
+def r_square(y_true: list[Union[float, int]], 
+            y_predict: list[Union[float, int]]) ->float:
     mean_y_true = sum(y_true)/len(y_true)
     numerator = sum((y_true[i] - y_predict[i]) ** 2 for i in range(len(y_true)))
     denominator = sum((y_true[i] - mean_y_true) ** 2 for i in range(len(y_true)))
     return 1 - (numerator/denominator)
 
-def accuracy(y_true: list, y_predict: list) ->float:
+
+def Mean_Squared_Error(y_true: list[Union[float, int]], 
+                    y_predict: list[Union[float, int]]) ->float:
+    mse = sum((y_true[i] - y_predict[i])**2 for i in range(len(y_true))) / len(y_true)
+    return mse
+
+
+def Mean_Absolute_Error(y_true: list[Union[float, int]], 
+                        y_predict: list[Union[float, int]]) ->float:
+    mae = sum(abs(y_true[i] - y_predict[i]) for i in range(len(y_true))) / len(y_true)
+    return mae
+
+
+def accuracy(y_true: list[Union[float, int]], 
+            y_predict: list[Union[float, int]]) ->float:
     count = 0
     for a,b in zip(y_true,y_predict):
         if a==b:
             count +=1
     return count/len(y_true)
+
+
+def precision(y_true: list[Union[float, int]], 
+              y_predict: list[Union[float, int]], 
+              average: Optional[str] = None) -> float:
+    """
+    Calculate the precision for the given true labels and predicted labels.
+
+    Args:
+        y_true (List[Union[float, int]]): True labels.
+        y_predict (List[Union[float, int]]): Predicted labels.
+        average (Optional[str]): Type of averaging performed on the data. 
+                                 "macro" or "micro" or None (default is None).
+
+    Returns:
+        float: Calculated precision.
+    """
+    precision_scores = {}
+    if average not in (None, "macro", "micro"):
+        raise ValueError("Average must be one of None, 'macro', or 'micro'")
+    
+    classess = set(y_true)
+    # calculate precision score for binary classification
+    if average is None:
+        TP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==yp==1))
+        FP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==0 and yp==1))
+        precision_score = TP/(TP+FP) if (TP+FP) > 0 else 0
+    
+    else:
+        for c in classess:
+            precision_c = f'precision_{c}'
+            TP = sum((1 for yt,yp in zip(y_true, y_predict) if c==yt and c==yp))
+            FP = sum((1 for yt,yp in zip(y_true, y_predict) if c!=yt and c==yp))
+            precision_scores[precision_c] = TP/(TP+FP) if (TP+FP) > 0 else 0
+                                                    
+        if average == "macro":
+            # Macro-averaged precision
+            precision_score = sum(precision_scores.values())/len(classess)
+  
+    return precision_score
+
+    
+
+
 
 # def F1_Score(y_true: list, y_predict: list) ->float:
 #     TP = 0
--- tests/test_metrics/test_metrics.py
+++ tests/test_metrics/test_metrics.py
@@ -1,5 +1,6 @@
-from metrics.metrics import r_square, accuracy, F1_Score
-from sklearn.metrics import r2_score, accuracy_score, f1_score
+import math
+from metrics.metrics import r_square, accuracy, Mean_Squared_Error, Mean_Absolute_Error, precision
+from sklearn.metrics import r2_score, accuracy_score, mean_squared_error, mean_absolute_error, precision_score, f1_score
 
 
 def test_r_square():
@@ -9,6 +10,19 @@ def test_r_square():
     expected_output = r2_score(y_true, y_predict)
     assert output == expected_output
               
+def test_Mean_Squared_Error():
+    y_true = [3,-0.5,2,7,4.2,1.5,3.3,2.8,1.2,4.6]
+    y_predict = [2.5,0.0,2,8,4.0,1.7,3.5,2.6,1.4,4.8]
+    mse = Mean_Squared_Error(y_true=y_true, y_predict=y_predict)
+    expected_output = mean_squared_error(y_true,y_predict)
+    assert math.isclose(mse,expected_output)
+
+def test_Mean_Absolute_Error():
+    y_true = [3,-0.5,2,7,4.2,1.5,3.3,2.8,1.2,4.6]
+    y_predict = [2.5,0.0,2,8,4.0,1.7,3.5,2.6,1.4,4.8]
+    mae = Mean_Absolute_Error(y_true=y_true, y_predict=y_predict)
+    expected_output = mean_absolute_error(y_true, y_predict)
+    assert math.isclose(mae,expected_output)
 
 def test_accuracy():
     y_true = [1,0,1,1,0,1,0,0,1,0]
@@ -16,6 +30,20 @@ def test_accuracy():
     score = accuracy(y_true=y_true, y_predict=y_predict)
     expected_score = accuracy_score(y_true, y_predict)
     assert score == expected_score
+
+
+def test_precision():
+    y_true = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2]
+    y_predict = [0, 0, 1, 1, 1, 2, 0, 1, 2, 2]
+    score = precision(y_true=y_true, y_predict=y_predict, average="macro")
+    expected_output = precision_score(y_true, y_predict, average="macro")
+    y_true1 =    [1,0,1,1,0,1,0,0,1,0]
+    y_predict1 = [1,0,1,0,0,1,0,1,1,0]
+    score1 = precision(y_true=y_true1, y_predict=y_predict1)
+    expected_output1 = precision_score(y_true1, y_predict1,average='binary')
+    assert math.isclose(score,expected_output)
+    assert math.isclose(score1,expected_output1)
+
 
 # def test_F1_Score():
 #     y_true = [1,0,1,1,0,1,0,0,1,0]
