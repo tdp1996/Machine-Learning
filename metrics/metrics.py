ef3ba1b1c220968053b68ec07ac67c313781acf2
tdp1996 <phuongtd1107@gmail.com>
2024-07-05 16:08:17 +0700

    add f1_score function

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -29,7 +29,7 @@ def accuracy(y_true: list[Union[float, int]],
     return count/len(y_true)
 
 
-def precision(y_true: list[Union[float, int]], 
+def Precision_score(y_true: list[Union[float, int]], 
               y_predict: list[Union[float, int]], 
               average: Optional[str] = None) -> float:
     """
@@ -45,16 +45,19 @@ def precision(y_true: list[Union[float, int]],
         float: Calculated precision.
     """
     precision_scores_dict = {}
+    precision_score = 0
     if average not in (None, "macro", "micro"):
         raise ValueError("Average must be one of None, 'macro', or 'micro'")
     
     classess = set(y_true)
+
     # calculate precision score for binary classification
     if average is None:
         TP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==yp==1))
         FP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==0 and yp==1))
         precision_score = TP/(TP+FP) if (TP+FP) > 0 else 0
     
+    # calculate precision score for multiclass classification
     else:
         for c in classess:
             precision_c = f'precision_{c}'
@@ -74,15 +77,115 @@ def precision(y_true: list[Union[float, int]],
   
     return precision_score
 
+
+def Recall_score(y_true: list[Union[float, int]], 
+            y_predict: list[Union[float, int]], 
+            average: Optional[str] = None) -> float:
+    """
+    Calculate the recall for the given true labels and predicted labels.
+
+    Args:
+        y_true (List[Union[float, int]]): True labels.
+        y_predict (List[Union[float, int]]): Predicted labels.
+        average (Optional[str]): Type of averaging performed on the data. 
+                                 "macro" or "micro" or None (default is None).
+
+    Returns:
+        float: Calculated recall score.
+    """
+
+    recall_score = 0
+    recall_scores_dict = {}
+
+    if average not in (None, "macro", "micro"):
+        raise ValueError("Average must be one of None, 'macro', or 'micro'")
     
+    classess = set(y_true)
+
+    # calculate recall score for binary classification
+    if average is None:
+        TP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==yp==1))
+        FN = sum((1 for yt,yp in zip(y_true, y_predict) if yt==1 and yp==0))
+        recall_score = TP/(TP+FN) if (TP+FN) > 0 else 0
+
+    # calculate recall score for multiclass classification    
+    else:
+        for c in classess:
+            recall_c = f'recall_{c}'
+            TP = sum((1 for yt,yp in zip(y_true, y_predict) if c==yt and c==yp))
+            FN = sum((1 for yt,yp in zip(y_true, y_predict) if c==yt and c!=yp))
+            recall_scores_dict[recall_c] = TP/(TP+FN) if (TP+FN) > 0 else 0
+
+        # Macro-averaged recall
+        if average == "macro":
+            recall_score = sum(recall_scores_dict.values()) / len(classess)
+
+        # Micro-averaged recall
+        elif average == "micro":
+            TP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==yp))
+            FN = sum((1 for yt,yp in zip(y_true, y_predict) if yt!=yp))
+            recall_score = TP/(TP+FN) if (TP+FN) > 0 else 0
+            
+    return recall_score
 
 
 
-# def F1_Score(y_true: list, y_predict: list) ->float:
-#     TP = 0
-#     TN = 0
-#     FP = 0
-#     FN = 1
-#     for a,b in zip(y_true, y_predict):
-#         if a==1 and 
+def F1_score(y_true: list[Union[float, int]], 
+            y_predict: list[Union[float, int]], 
+            average: Optional[str] = None) -> float:
+    """
+    Calculate the F1 score for the given true labels and predicted labels.
+
+    Args:
+        y_true (list[Union[float, int]]): True labels.
+        y_predict (list[Union[float, int]]): Predicted labels.
+        average (Optional[str]): Type of averaging performed on the data. 
+                                 "macro", "micro" or None (default is None).
+
+#     Returns:
+#         float: Calculated F1 score.
+#     """
+
+    
+    if average not in (None, "macro", "micro"):
+        raise ValueError("Average must be one of None, 'macro', or 'micro'")
+
+    # calculate precision score for binary classification
+    if average is None:
+        TP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==yp==1))
+        FP = sum((1 for yt,yp in zip(y_true, y_predict) if yt==0 and yp==1))
+        FN = sum((1 for yt,yp in zip(y_true, y_predict) if yt==1 and yp==0))
+        precision = TP/(TP+FP) if (TP+FP) > 0 else 0
+        recall = TP/(TP+FN) if (TP+FN) > 0 else 0
+        f1_score = 2 * ((precision * recall) / (precision + recall))
+
+    # calculate precision score for multiclass classification
+    else:
+        classess = set(y_true)
+        precision_score = []
+        recall_score = []
+        sum_TP = 0
+        sum_FP = 0
+        sum_FN = 0
+
+        for c in classess:
+            TP = sum((1 for yt,yp in zip(y_true, y_predict) if c==yt and c==yp))
+            FP = sum((1 for yt,yp in zip(y_true, y_predict) if c!=yt and c==yp))
+            FN = sum((1 for yt,yp in zip(y_true, y_predict) if c==yt and c!=yp))
+            precision_score.append( TP/(TP+FP) if (TP+FN) > 0 else 0)
+            recall_score.append(TP/(TP+FN) if (TP+FN) > 0 else 0)
+            sum_TP +=TP
+            sum_FP +=FP
+            sum_FN +=FN
+
+        if average == "macro":
+            f1_score = sum(2*(p * r)/(p+r) if (p+r)!=0 else 0 for p,r in zip(precision_score,recall_score)) / len(classess)
+    
+        if average == "micro":
+            precision = sum_TP / (sum_TP + sum_FP) if (sum_TP + sum_FP)!=0 else 0
+            recall = sum_TP / (sum_TP + sum_FN) if (sum_TP + sum_FN)!=0 else 0
+            f1_score = 2*(precision*recall)/(precision+recall) if (precision+recall)!=0 else 0
+    
+    return f1_score
+
     
--- tests/test_metrics/test_metrics.py
+++ tests/test_metrics/test_metrics.py
@@ -1,6 +1,6 @@
 import math
-from metrics.metrics import r_square, accuracy, Mean_Squared_Error, Mean_Absolute_Error, precision
-from sklearn.metrics import r2_score, accuracy_score, mean_squared_error, mean_absolute_error, precision_score, f1_score
+from metrics.metrics import r_square, accuracy, Mean_Squared_Error, Mean_Absolute_Error, Precision_score, Recall_score, F1_score
+from sklearn.metrics import r2_score, accuracy_score, mean_squared_error, mean_absolute_error, precision_score,recall_score, f1_score
 
 
 def test_r_square():
@@ -35,26 +35,54 @@ def test_accuracy():
 def test_precision():
     y_true = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2]
     y_predict = [0, 0, 1, 1, 1, 2, 0, 1, 2, 1]
-    score_macro = precision(y_true=y_true, y_predict=y_predict, average="macro")
-    expected_output_macro = precision_score(y_true, y_predict, average="macro")
+    precision_score_macro = Precision_score(y_true=y_true, y_predict=y_predict, average="macro")
+    expected_precision_score_macro = precision_score(y_true, y_predict, average="macro")
 
-    score_micro = precision(y_true=y_true, y_predict=y_predict, average="micro")
-    expected_output_micro = precision_score(y_true, y_predict, average="micro")
+    precision_score_micro = Precision_score(y_true=y_true, y_predict=y_predict, average="micro")
+    expected_precision_score_micro = precision_score(y_true, y_predict, average="micro")
 
     y_true_binary =  [1,0,1,1,0,1,0,0,1,0]
     y_predict_binary = [1,0,1,0,0,1,0,1,1,0]
-    score_binary = precision(y_true=y_true_binary, y_predict=y_predict_binary)
-    expected_output_binary = precision_score(y_true_binary, y_predict_binary,average='binary')
+    precision_score_binary = Precision_score(y_true=y_true_binary, y_predict=y_predict_binary)
+    expected_precision_score_binary = precision_score(y_true_binary, y_predict_binary)
 
-    assert math.isclose(score_macro,expected_output_macro)
-    assert math.isclose(score_micro,expected_output_micro)
-    assert math.isclose(score_binary,expected_output_binary)
+    assert math.isclose(precision_score_macro,expected_precision_score_macro)
+    assert math.isclose(precision_score_micro,expected_precision_score_micro)
+    assert math.isclose(precision_score_binary,expected_precision_score_binary)
 
+def test_Recall_score():
+    y_true_binary =  [1,0,1,1,0,1,0,0,1,0]
+    y_predict_binary = [1,0,1,0,0,1,0,1,1,0]
+    recall_score_binary = Recall_score(y_true=y_true_binary, y_predict=y_predict_binary)
+    expected_recall_score_binary = recall_score(y_true_binary, y_predict_binary)
+    
+    y_true = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2]
+    y_predict = [0, 0, 1, 1, 1, 2, 0, 1, 2, 1]
+    recall_score_macro = Recall_score(y_true=y_true, y_predict=y_predict, average="macro")
+    expected_recall_score_macro = recall_score(y_true, y_predict, average="macro")
 
-# def test_F1_Score():
-#     y_true = [1,0,1,1,0,1,0,0,1,0]
-#     y_predict = [1,0,1,0,0,1,0,1,1,0]
-#     score = F1_Score(y_true=y_true, y_predict=y_predict)
-#     expected_score = f1_score(y_true, y_predict)
-#     assert score == expected_score
+    recall_score_micro = Recall_score(y_true=y_true, y_predict=y_predict, average="micro")
+    expected_recall_score_micro = recall_score(y_true, y_predict, average="micro")
+
+    assert math.isclose(recall_score_binary,expected_recall_score_binary)
+    assert math.isclose(recall_score_macro, expected_recall_score_macro)
+    assert math.isclose(recall_score_micro, expected_recall_score_micro)
+
+def test_F1_score():
+    y_true_binary =    [1,0,1,1,0,1,0,0,1,0]
+    y_predict_binary = [1,0,1,0,0,1,0,1,1,0]
+    f1_score_binary = F1_score(y_true=y_true_binary, y_predict=y_predict_binary)
+    expected_f1_score_binary = f1_score(y_true_binary, y_predict_binary)
+    
+    y_true = [0, 0, 0, 1, 1, 1, 2, 2, 2, 2]
+    y_predict = [0, 0, 1, 1, 1, 2, 0, 1, 2, 1]
+    f1_score_macro = F1_score(y_true=y_true, y_predict=y_predict, average="macro")
+    expected_f1_score_macro = f1_score(y_true, y_predict, average="macro")
+
+    f1_score_micro = F1_score(y_true=y_true, y_predict=y_predict, average="micro")
+    expected_f1_score_micro = f1_score(y_true, y_predict, average="micro")
+
+    assert math.isclose(f1_score_binary, expected_f1_score_binary)
+    assert math.isclose(f1_score_macro, expected_f1_score_macro)
+    assert math.isclose(f1_score_micro, expected_f1_score_micro)
 
