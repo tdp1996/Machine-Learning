d049dbc03ea6155a326f0e95f053142b766ddb7b
tdp1996 <phuongtd1107@gmail.com>
2024-07-10 22:44:48 +0700

    add dot matrix function

--- homemade/utils/operations.py
+++ homemade/utils/operations.py
@@ -51,6 +51,44 @@ def subtract_matrix(x1: list[list[Union[int, float]]],
 
     return result
 
+
 def dot_matrix(x1: list[list[Union[int, float]]], 
                 x2: Union[int,float,list[Union[int,float]],list[list[Union[int, float]]]]) -> list[list[Union[int,float]]]:
-    pass
+    """
+    Perform the dot product of two matrices or a matrix and a scalar/vector.
+
+    Args:
+        x1 (list of list of Union[int, float]): The first matrix.
+        x2 (Union[int, float, list of Union[int, float], list of list of Union[int, float]]): The second operand which can be 
+        a scalar, vector, or matrix.
+
+    Returns:
+        list of list of Union[int, float]: The result of the dot product operation.
+
+    Raises:
+    ValueError: If the dimensions of the matrices are not compatible for multiplication.
+    """
+    
+    if isinstance(x2,(int,float)):
+        result = [[item[i] * x2 for i in range(len(item))] for item in x1]
+    
+    elif all(isinstance(item,(int,float)) for item in x2):
+        result = [sum(item[i]*x2[i] for i in range(len(x2))) for item in x1]
+
+    else:
+        rows_x1 = len(x1)
+        cols_x1 = len(x1[0])
+        cols_x2 = len(x2[0])
+        if rows_x1!=cols_x2:
+            raise ValueError("The rows of x1 and the cols of x2 must be same ")
+        # Initialize the result matrix with zeros
+        result = [[0 for _ in range(cols_x2)] for _ in range(rows_x1)]
+
+        # Perform matrix multiplication
+        for i in range(rows_x1):
+            for j in range(cols_x2):
+                for k in range(cols_x1):
+                    result[i][j] += x1[i][k] * x2[k][j]
+
+    return result
+        
--- tests/test_homemade/test_utils/test_operations.py
+++ tests/test_homemade/test_utils/test_operations.py
@@ -10,15 +10,15 @@ def test_add_matrix():
     x4 = [[10, 20, 30],
           [40, 50, 60]]
     
-    output1 = add_matrix(x1,x2)
+    output1 = add_matrix(x1=x1,x2=x2)
     expected_output1 = np.add(x1,x2)
     assert np.allclose(output1,expected_output1)
 
-    output2 = add_matrix(x1,x3)
+    output2 = add_matrix(x1=x1,x2=x3)
     expected_output2 = np.add(x1,x3)
     assert np.allclose(output2,expected_output2)
 
-    output3 = add_matrix(x1,x4)
+    output3 = add_matrix(x1=x1,x2=x4)
     expected_output3 = np.add(x1,x4)
     assert np.allclose(output3,expected_output3)
 
@@ -30,15 +30,34 @@ def test_subtract_matrix():
     x4 = [[10, 20, 30],
           [40, 50, 60]]
     
-    output1 = subtract_matrix(x1,x2)
-    expected_output1 = np.subtract(x1,x2)
+    output1 = subtract_matrix(x1=x1,x2=x2)
+    expected_output1 = np.subtract(x1=x1,x2=x3)
     assert np.allclose(output1,expected_output1)
 
-    output2 = subtract_matrix(x1,x3)
+    output2 = subtract_matrix(x1=x1,x2=x3)
     expected_output2 = np.subtract(x1,x3)
     assert np.allclose(output2,expected_output2)
 
-    output3 = subtract_matrix(x1,x4)
+    output3 = subtract_matrix(x1=x1,x2=x4)
     expected_output3 = np.subtract(x1,x4)
+    assert np.allclose(output3,expected_output3)
+
+def test_dot_matrix():
+    x1 = [[1, 2, 3],
+          [4, 5, 6]]
+    x2 = 3
+    x3 = [-2, 0.5, 1.5]
+    x4 = [[7, 8], [9, 10], [11, 12]]
+
+    output1 = dot_matrix(x1=x1, x2=x2)
+    expected_output1 = np.dot(x1,x2)
+    assert np.allclose(output1,expected_output1)
+
+    output2 = dot_matrix(x1=x1,x2=x3)
+    expected_output2 = np.dot(x1,x3)
+    assert np.allclose(output2,expected_output2)
+
+    output3 = dot_matrix(x1=x1,x2=x4)
+    expected_output3 = np.dot(x1,x4)
     assert np.allclose(output3,expected_output3)
     
