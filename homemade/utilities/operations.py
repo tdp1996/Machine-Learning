586e247e57646aabc51e35107f3b08e0e1a57922
tdp1996 <phuongtd1107@gmail.com>
2024-07-10 20:51:34 +0700

    add subtract matrix function

--- homemade/utilities/operations.py
+++ homemade/utilities/operations.py
@@ -8,7 +8,7 @@ def add(x1: list[list[Union[int, float]]],
     Args:
          x1: A 2D list (matrix) of integers or floats.
          x2: An integer, float, 1D list (vector), or 2D list (matrix) of integers or floats.
-         
+
     Return: A 2D list (matrix) where x2 is added to x1.
     """
 
@@ -26,4 +26,27 @@ def add(x1: list[list[Union[int, float]]],
     return result
 
     
+def subtract(x1: list[list[Union[int, float]]], 
+            x2: Union[int,float,list[Union[int,float]],list[list[Union[int, float]]]]) -> list[list[Union[int,float]]]:
+    """
+    Subtracts a scalar, vector, or matrix to another matrix.
 
+    Args:
+         x1: A 2D list (matrix) of integers or floats.
+         x2: An integer, float, 1D list (vector), or 2D list (matrix) of integers or floats.
+
+    Return: A 2D list (matrix) where x2 is subtracted to x1.
+    """
+
+    if isinstance(x2,(int,float)):
+        result = [[item[i] - x2 for i in range(len(item))] for item in x1]
+    elif isinstance(x2,list) and all(isinstance(item,(int,float)) for item in x2):
+        result = [[(x1[i][j] - x2[j]) for j in range(len(x2))] for i in range(len(x1))]
+    else:
+        num_rows = len(x1)
+        num_cols = len(x1[0])
+        if len(x2) != num_rows or any(len(row)!=num_cols for row in x2):
+            raise ValueError("Two matrices must have the same dimensions") 
+        result = [[x1[i][j] - x2[i][j] for j in range(num_cols)] for i in range(num_rows)]
+
+    return result
--- tests/test_homemade/test_utilities/test_operations.py
+++ tests/test_homemade/test_utilities/test_operations.py
@@ -1,5 +1,5 @@
 import numpy as np
-from homemade.utilities.operations import add
+from homemade.utilities.operations import add, subtract
 
 
 def test_add():
@@ -20,5 +20,25 @@ def test_add():
 
     output3 = add(x1,x4)
     expected_output3 = np.add(x1,x4)
+    assert np.allclose(output3,expected_output3)
+
+def test_subtract():
+    x1 = [[1, 2, 3], 
+         [4, 5, 6]]
+    x2 = 3
+    x3 = [0.5, 2, -3]
+    x4 = [[10, 20, 30],
+          [40, 50, 60]]
+    
+    output1 = subtract(x1,x2)
+    expected_output1 = np.subtract(x1,x2)
+    assert np.allclose(output1,expected_output1)
+
+    output2 = subtract(x1,x3)
+    expected_output2 = np.subtract(x1,x3)
+    assert np.allclose(output2,expected_output2)
+
+    output3 = subtract(x1,x4)
+    expected_output3 = np.subtract(x1,x4)
     assert np.allclose(output3,expected_output3)
     
